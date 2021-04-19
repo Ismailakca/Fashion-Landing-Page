a1098053c1d9e98afd808d82a663dfce4d24cd0a
ismailakca <ismaillakkca@gmail.com>
2021-04-20 02:05:37 +0300

    slides

--- js/main.js
+++ js/main.js
@@ -4,3 +4,20 @@ const menu = document.querySelector('.Header__navigation')
 buton.addEventListener('click', () => {
    menu.classList.toggle('is_show')
 })
+
+$(document).ready(function(){
+   $('.Women__slider').slick({
+      arrows:false,
+      slidesToShow: 4,
+      autoplay:true,
+      slidesToScroll: 1,
+      responsive: [
+         {
+           breakpoint: 768,
+           settings: {
+             slidesToShow: 2
+           }
+         }
+      ]
+   })
+})
